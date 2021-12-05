888c9c53677294bd6553c3bf191f4bff5618431f
RuthVR <ruth.vergarareyes.450@my.csun.edu>
2021-12-05 08:40:06 -0800

    second time updating

--- group_project/script/cleaning_up_data.R
+++ group_project/script/cleaning_up_data.R
@@ -8,8 +8,27 @@ library(janitor)
 
 ## Loading Data ################################################################ 
 
-Temperature_data <- read_csv(here("group_project/data/water_temp_data_2012_2021.csv"))
+Temperature_data <- read_csv(here("group_project/data/temperature_data_water.csv"))
 Zooplankton_data <- read_csv(here("group_project/data/Zooplankton_Abundance_Latest_raw_data.csv"))
 Phytoplankton_data <- read_csv(here("group_project/data/Phytoplankton_Abundance_Latest_raw_data.csv"))
 
 ## Cleaning up the data ########################################################
+Temp_data_clean <- Temperature_data %>%
+  clean_names() %>%
+  select(-date, -time, -seconds_since_1970)%>%
+  separate(
+    col = date_time,  
+    into = c('date', 'time'),  
+    sep = " ") %>%
+  select(-time) %>%
+  separate(
+    col = date,
+    into = c('year', 'month', 'day'), 
+    sep = "-"  ) %>%
+  filter(year != '2013',
+         year != '2014', 
+         year != '2021') %>%
+  rename(average_water_temp_C = water_temperature_6_0m_agincourt_reef_weather_station_level0_value_avg)
+
+Zooplankton_cleaned_data
+
