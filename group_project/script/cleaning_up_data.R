1091ff124e311995976c5e56b3e48ec90ae081dc
RuthVR <ruth.vergarareyes.450@my.csun.edu>
2021-12-05 10:36:56 -0800

    updating the data selected

--- group_project/script/cleaning_up_data.R
+++ group_project/script/cleaning_up_data.R
@@ -9,8 +9,7 @@ library(janitor)
 ## Loading Data ################################################################ 
 
 Temperature_data <- read_csv(here("group_project/data/temperature_data_water.csv"))
-Zooplankton_data <- read_csv(here("group_project/data/Zooplankton_Abundance_Latest_raw_data.csv"))
-Phytoplankton_data <- read_csv(here("group_project/data/Phytoplankton_Abundance_Latest_raw_data.csv"))
+
 
 ## Cleaning up the data ########################################################
 Temp_data_clean <- Temperature_data %>%
@@ -28,7 +27,13 @@ Temp_data_clean <- Temperature_data %>%
   filter(year != '2013',
          year != '2014', 
          year != '2021') %>%
-  rename(average_water_temp_C = water_temperature_6_0m_agincourt_reef_weather_station_level0_value_avg)
+  rename(average_water_temp_C = water_temperature_6_0m_agincourt_reef_weather_station_level0_value_avg)%>%
+  unite(col = "year_month",   # the name of the NEW col #
+        c(year, month),       # the columns to unite #
+        sep = "_",            # lets put a . in the middle #
+        remove = FALSE) %>%   # keep the original columns #
+  write_csv(here("group_project", "data", "Cleaned_up_temperature_data.csv"))
 
-Zooplankton_cleaned_data
+
+  
 
